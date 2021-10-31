dc3e085ec19b1516e61d0ebac02004535e69c120
DavidKibetRono <david.rono@student.moringaschool.com>
2021-10-31 20:46:25 +0300

    Add edit button to default li

--- to-do.js
+++ to-do.js
@@ -8,7 +8,15 @@ try {
     myDft.appendChild(myDftNode)
     myDft.className = "delete"
     list.appendChild(myDft)
+    //Add edit button to default li
+    var list = document.getElementById("mydefault")
 
+    var myDft = document.createElement("span")
+    var myDftNode = document.createTextNode('Edit');
+
+    myDft.appendChild(myDftNode)
+    myDft.className = "edit"
+    list.appendChild(myDft) 
     
     
 
